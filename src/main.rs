8c11e3382033d2f96870d612a316f6fd3fd709bf
AalyTokombaev <alexiver96@hotmail.com>
2023-12-11 14:27:01 +0100

    bug in bitshift

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,8 @@ fn main() {
         gonna use the primitive poly x^3 + x + 1 for now
         this is represented by the string 1011, we cut off the first 1 getting 011
     */
-    let primitive_poly = vec![GF2::new(0), GF2::new(1), GF2::new(1)];
-    let field = Field::new(3, primitive_poly);
+    let primitive_poly = vec![GF2::new(0), GF2::new(0), GF2::new(1), GF2::new(1)];
+    let field = Field::new(4, primitive_poly);
     println!("{}", field);
 
     /*
@@ -18,7 +18,7 @@ fn main() {
     let x2: FieldElement = field.get_random_element();
     */ 
     let x1: FieldElement = field.from_integer(5);
-    let x2: FieldElement = field.from_integer(1);
+    let x2: FieldElement = field.from_integer(3);
 
     let _y1 : FieldElement = field.from_integer(4);
     let _y2 : FieldElement = field.from_integer(5);
